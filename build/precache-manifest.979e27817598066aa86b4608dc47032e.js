4eba8025e29fa8272bb79b99f2d88d30fd5dd252
johnny-whale <masha.volk1415@gmail.com>
2020-08-20 19:51:02 +0300

    Исправлены ошибки

---
+++ build/precache-manifest.979e27817598066aa86b4608dc47032e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888165a46919e72265b30445315692c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d9fa4edf56cf061fdad",
+    "url": "/static/css/2.8c6566a3.chunk.css"
+  },
+  {
+    "revision": "fec1b3b81b5fb27b5b6c",
+    "url": "/static/css/main.7f2218d0.chunk.css"
+  },
+  {
+    "revision": "5d9fa4edf56cf061fdad",
+    "url": "/static/js/2.157ef48f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.157ef48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec1b3b81b5fb27b5b6c",
+    "url": "/static/js/main.2bf9625b.chunk.js"
+  },
+  {
+    "revision": "117cd013736ddfcdf677",
+    "url": "/static/js/runtime-main.56c779cc.js"
+  },
+  {
+    "revision": "612b81d704360b1286c9aae62b07e0f1",
+    "url": "/static/media/card.612b81d7.jpg"
+  },
+  {
+    "revision": "b50aef354b5caa265b4f7a8f71ef1e97",
+    "url": "/static/media/main_back.b50aef35.jpg"
+  }
+]);
